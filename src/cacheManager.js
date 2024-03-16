8dcda729595b62c311c5539b92cf2368bb33e462
onedionys <onedionys@gmail.com>
2024-03-16 21:10:50 +0700

    chore: fixing space in between function

--- src/cacheManager.js
+++ src/cacheManager.js
@@ -2,19 +2,15 @@ class CacheManager {
     constructor() {
         this.cache = {};
     }
-
     set(key, value) {
         this.cache[key] = value;
     }
-
     get(key) {
         return this.cache[key];
     }
-
     delete(key) {
         delete this.cache[key];
     }
-
     clear() {
         this.cache = {};
     }
